2a313e3c375ac4549afb52be8773cd214dc1ba55
Gucci Fan <gucc@umich.edu>
2023-01-15 17:57:14 -0500

    commit tree 83472bb2794718b937ffad8b19dcdd3ea629b72a

--- app/models/freebie.rb
+++ app/models/freebie.rb
@@ -1,3 +1,13 @@
 class Freebie < ActiveRecord::Base
+    belongs_to :dev
+    belongs_to :company
 
+    def dev=(dev)
+        self.dev_id = dev.id 
+        self.save
+    end
+
+    def print_details
+        "#{self.dev.name} owns a #{self.item_name} from #{self.company.name}"
+    end
 end
